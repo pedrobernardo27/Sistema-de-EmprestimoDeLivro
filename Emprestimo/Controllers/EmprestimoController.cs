690f3e3332dd2c9feda8d2b20b6516286733b7a2
pedrobernardo27 <Josepedrobernardo@hotmail.com>
2023-11-01 10:28:48 -0300

    Add project files.

---
+++ Emprestimo/Controllers/EmprestimoController.cs
@@ -0,0 +1,117 @@
+﻿using Emprestimo.Data;
+using Emprestimo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Emprestimo.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimoModel> empretismo = _db.Emprestimos;
+            return View(empretismo);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimoModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimo);
+                _db.SaveChanges();
+
+                TempData["MensagemSucesso"] = "Cadastro realizado com sucesso!";
+
+                return RedirectToAction("Index");
+            }
+
+            TempData["MensagemErro"] = "Erro ao cadastrar!";
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimoModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.ID == id);
+
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimoModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+
+                TempData["MensagemSucesso"] = "Edição realizado com sucesso!";
+
+                return RedirectToAction("Index");
+            }
+
+            TempData["MensagemErro"] = "Erro ao editar";
+
+            return View(emprestimo);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimoModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.ID == id);
+
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimoModel emprestimo)
+        {
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+
+            TempData["MensagemSucesso"] = "Remoção realizadaS com sucesso!";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Emprestimo/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Emprestimo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Emprestimo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> optins) : base(optins)
+        {            
+        }
+
+        public DbSet<EmprestimoModel> Emprestimos { get; set; }
+    }
+}
